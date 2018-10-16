3e782f3484bdca949befd1810701fa3aa11d2098
Vitaly Khabarov <vitkhab@gmail.com>
2018-10-17 00:35:07 +0300

    Fix lint check on tests

--- molecule/resources/tests/zabbix-server/test_zabbix_server.py
+++ molecule/resources/tests/zabbix-server/test_zabbix_server.py
@@ -4,5 +4,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('zabbix_server')
 
+
 def test_installed_packages(host):
     assert host.package("zabbix-server-pgsql").is_installed
